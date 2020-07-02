d5d9b860ae4ed06c7b802b59f3fff30d58f77876
Alexey Stepanov <alexey.stepanov@graph.uk>
2020-07-02 13:10:25 +0300

    fixed vm and nic naming convention

--- main.tf
+++ main.tf
@@ -464,7 +464,7 @@ module "persona-3" {
   resource_group_name = azurerm_resource_group.vdi_resource_group.name
   azure_region        = azurerm_resource_group.vdi_resource_group.location
 
-  vm_name                     = "Win10Nv24SSD"
+  vm_name                     = "vmWin10Nv24-${var.deployment_index}"
   base_name                   = var.base_name
   image_id                    = var.golden_image_id
   pcoip_registration_code     = var.pcoip_registration_code
