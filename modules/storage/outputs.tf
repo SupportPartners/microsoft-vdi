31ab74c07f60efe68206c90647505e6733e2fc25
Alexey Stepanov <alexey.stepanov@graph.uk>
2020-07-02 13:23:06 +0300

    changed storage account for VM diagnostic

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -11,5 +11,5 @@ output "storage_access_key" {
 }
 
 output "diag_storage_blob_endpoint" {
-  value = local.storage[0].primary_blob_endpoint
+  value = azurerm_storage_account.diagnostic-storage-account.primary_blob_endpoint
 }
